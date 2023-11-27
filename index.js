3c7f2821f4694f17a0479dbb7b4f3e65fc67bf70
Priyanka-Das-Dipa <priyankadipa9102@gmail.com>
2023-11-28 00:23:11 +0600

    add the user apies

--- index.js
+++ index.js
@@ -27,7 +27,9 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
-    const articleCollection = client.db("newsPaperDB").collection("allArticles");
+    const articleCollection = client
+      .db("newsPaperDB")
+      .collection("allArticles");
     const userCollection = client.db("newsPaperDB").collection("users");
 
     app.post("/jwt", async (req, res) => {
@@ -35,15 +37,46 @@ async function run() {
       const token = jwt.sign(user, process.env.ACCESS_TOKEN, {
         expiresIn: "1h",
       });
-      res.send({token})
+      res.send({ token });
     });
+
+    const verifyToken = (req, res, next) => {
+      console.log('inside verify token',req.headers.authorization);
+      if(!req.headers.authorization){
+        return res.status(401).send({message: 'forbidden access'})
+      }
+      const token = req.headers.authorization.split(' ')[1];
+      jwt.verify(token, process.env.ACCESS_TOKEN, (err, decoded)=>{
+        if(err){
+          return res.status(401).send({message: 'forbidden access'})
+        }
+        req.decoded = decoded;
+        next();
+      })
+    };
 
     app.get("/allArticles", async (req, res) => {
       const result = await articleCollection.find().toArray();
       res.send(result);
     });
 
-    app.get("/users", async (req, res) => {
+    app.get('/users/:email', verifyToken, async (req, res) => {
+      const email = req.params.email;
+
+      if (email !== req.decoded.email) {
+        return res.status(403).send({ message: 'forbidden access' })
+      }
+
+      const query = { email: email };
+      const user = await userCollection.findOne(query);
+      let admin = false;
+      if (user) {
+        admin = user?.role === 'admin';
+      }
+      res.send({ admin });
+    })
+
+    app.get("/users", verifyToken, async (req, res) => {
       const result = await userCollection.find().toArray();
       res.send(result);
     });
